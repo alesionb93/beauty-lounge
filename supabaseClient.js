5db50c4cfc934b699849c79bb4bf0a055627d0d0
alesionb93 <alesio.barreiro@eyemobile.com.br>
2026-04-12 11:26:08 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,5 @@
-import { createClient } from '@supabase/supabase-js'
+// supabaseClient.js
+const SUPABASE_URL = 'https://dtslpuqhvyamdittevul.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0c2xwdXFodnlhbWRpdHRldnVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5NzA0MTcsImV4cCI6MjA5MTU0NjQxN30.wjQusHU566_2s9piC9qO-fqCPIJVrXPbd7YEfEzXl_U'
 
-const supabaseUrl = 'https://dtslpuqhvyamdittevul.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0c2xwdXFodnlhbWRpdHRldnVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5NzA0MTcsImV4cCI6MjA5MTU0NjQxN30.wjQusHU566_2s9piC9qO-fqCPIJVrXPbd7YEfEzXl_U'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
