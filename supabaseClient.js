a158792be330d8ab5589dadb3ee7eafe8441d861
alesionb93 <alesio.barreiro@eyemobile.com.br>
2026-04-12 18:13:17 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,4 @@
-// supabaseClient.js — SEM import/export, usa variável global
+// supabaseClient.js
 var SUPABASE_URL = 'https://dtslpuqhvyamdittevul.supabase.co';
 var SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0c2xwdXFodnlhbWRpdHRldnVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5NzA0MTcsImV4cCI6MjA5MTU0NjQxN30.wjQusHU566_2s9piC9qO-fqCPIJVrXPbd7YEfEzXl_U';
 
