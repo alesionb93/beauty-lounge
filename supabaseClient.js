c095521162e05c8509e4b2f0d82caf33029a07b1
alesionb93 <alesio.barreiro@eyemobile.com.br>
2026-04-12 17:32:25 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,5 @@
-// supabaseClient.js
-const SUPABASE_URL = 'https://dtslpuqhvyamdittevul.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0c2xwdXFodnlhbWRpdHRldnVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5NzA0MTcsImV4cCI6MjA5MTU0NjQxN30.wjQusHU566_2s9piC9qO-fqCPIJVrXPbd7YEfEzXl_U'
+// supabaseClient.js — SEM import/export, usa variável global
+var SUPABASE_URL = 'https://dtslpuqhvyamdittevul.supabase.co';
+var SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0c2xwdXFodnlhbWRpdHRldnVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5NzA0MTcsImV4cCI6MjA5MTU0NjQxN30.wjQusHU566_2s9piC9qO-fqCPIJVrXPbd7YEfEzXl_U';
 
-export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+var supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
